58748f15bf52cc5e023a1ae61bb9ac2405a80bb6
martimpmy <tu-correo@ejemplo.com>
2025-09-05 16:45:52 -0300

    agrego el RoleService

---
+++ Farmacia/src/main/java/com/example/uade/tpo/Farmacia/service/RoleService.java
@@ -0,0 +1,24 @@
+package com.example.uade.tpo.Farmacia.service;
+
+import com.example.uade.tpo.Farmacia.entity.User;
+import com.example.uade.tpo.Farmacia.entity.Role;
+import com.example.uade.tpo.Farmacia.repository.UserRepository;
+
+import exceptions.UserAlreadyExistsException;
+
+import com.example.uade.tpo.Farmacia.repository.RoleRepository;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+
+@Service
+public class RoleService {
+    
+}
