ef8eb0dc42077fddf7ab4ceec0009ab20525f682
martimpmy <tu-correo@ejemplo.com>
2025-09-05 18:24:17 -0300

    Repository de CartItem y Cart by; Cielo

--- Farmacia/src/main/java/com/example/uade/tpo/Farmacia/repository/CartItemRepository.java
+++ Farmacia/src/main/java/com/example/uade/tpo/Farmacia/repository/CartItemRepository.java
@@ -1,5 +1,9 @@
 package com.example.uade.tpo.Farmacia.repository;
 
-public class CartItemRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.uade.tpo.Farmacia.entity.CartItem;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
--- Farmacia/src/main/java/com/example/uade/tpo/Farmacia/repository/CartRepository.java
+++ Farmacia/src/main/java/com/example/uade/tpo/Farmacia/repository/CartRepository.java
@@ -1,5 +1,16 @@
 package com.example.uade.tpo.Farmacia.repository;
 
-public class CartRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.uade.tpo.Farmacia.entity.Cart;
+import com.example.uade.tpo.Farmacia.entity.CartItem;
+import com.example.uade.tpo.Farmacia.entity.User;
+
+@Repository
+public interface CartRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<Cart> findByUserAndStatus(User user, Cart.Status status);
 }
